b08f9246661f26569ccce5d6550eefeb96a31217
trungkien263 <trungkienvg263@gmail.com>
2021-09-15 00:57:43 +0700

    inital RN-map

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,6 @@ module.exports = {
   rules: {
     'react-native/no-inline-styles': 0,
     'prettier/prettier': 0,
+    'react/react-in-jsx-scope': 0,
   },
 };
--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import GlobalStyle from '../utils/GlobalStyle';
+import {StyleSheet, View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
-export default function Explore() {
-  return (
-    <View style={GlobalStyle.cernterView}>
-      <Text>Explore Screen</Text>
-    </View>
-  );
-}
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+const Explore = () => (
+  <View style={styles.container}>
+    <MapView
+      provider={PROVIDER_GOOGLE}
+      style={styles.map}
+      region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      }}
+    />
+  </View>
+);
+
+export default Explore;
